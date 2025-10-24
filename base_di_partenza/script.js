f7c59b266de164bfe5309ac44b954a6ed8242020
Hammers <Gianluca.hammers@gmail.com>
2025-10-24 16:39:00 +0200

    PERFECTION

--- base_di_partenza/script.js
+++ base_di_partenza/script.js
@@ -1,9 +1,22 @@
 const allg = document.querySelectorAll("svg g");
 
-const reroll = function () {
+/* const reroll = function () {
   allg.forEach((g) => {
     g.style.opacity = Math.random() < 0.5 ? "0" : "1";
   });
 };
 
-setInterval(reroll, 100);
+setInterval(reroll, 100); 
+*/
+
+const randomLetter = () => {
+  const g = allg[Math.floor(Math.random() * allg.length)];
+
+  g.style.opacity = "0";
+
+  setTimeout(() => {
+    g.style.opacity = "1";
+  }, 800);
+};
+
+setInterval(randomLetter, 50);
